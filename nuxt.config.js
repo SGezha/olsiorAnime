16b915f0777e62d707b35d2b474956c422475b11
Sasha <dr.uma228@hotmail.com>
2021-08-12 23:56:25 +0300

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,11 +17,6 @@ export default {
     ],
   },
 
-  server: {     
-    port: 3000, // default: 3000     
-    host: '0.0.0.0', // default: localhost   
-  },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~assets/css/global.css"
