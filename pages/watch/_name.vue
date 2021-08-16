9f5478129dc0856a55f89aae63d24bf12f115538
SGezha <sasha-gezha1@mail.ru>
2021-08-17 01:10:17 +0300

    new emotes

--- pages/watch/_name.vue
+++ pages/watch/_name.vue
@@ -295,7 +295,7 @@ export default {
   methods: {
     async getChat(url) {
       const emotes = await this.$axios.$get(
-        `https://cdn.glitch.com/513930f1-8551-4a01-b9f0-59a88e2429c1%2Femotes.json?v=1629032530736`
+        `https://cdn.glitch.com/513930f1-8551-4a01-b9f0-59a88e2429c1%2Femotes.json?v=1629151786366`
       );
       this.emotes = emotes;
       let chat = await this.$axios.$get(`${url}`);
