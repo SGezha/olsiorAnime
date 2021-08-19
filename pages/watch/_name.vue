c8c8f1eefb1e0daa0c77efc6ddcf8e85a31a542d
Sasha <sasha-gezha1@mail.ru>
2021-08-19 23:27:58 +0300

    bleach

--- pages/watch/_name.vue
+++ pages/watch/_name.vue
@@ -52,20 +52,19 @@
                 save.id,
                 anime.episodes[save.id].url,
                 anime.episodes[save.id].title,
-                anime.episodes[save.id].chat
+                anime.episodes[save.id].chat,
+                anime.episodes[save.id]
               )
             "
           >
-            <span
-              >{{ anime.episodes[save.id].title }} ({{
-                formatTime(save.time)
-              }})</span
-            >
+            <span>
+              {{ anime.episodes[save.id].title }} ({{ formatTime(save.time) }})
+            </span>
           </div>
         </div>
       </div>
 
-      <div class="player-block m-5 flex" :class="{ theatre: theatre }">
+      <div class="player-block m-5 flex md:flex-row" :class="{ theatre: theatre, 'flex-col': !theatre }">
         <div
           class="player"
           :class="{ hidden: video == null, theatre: theatre }"
@@ -120,69 +119,48 @@
         </div>
       </div>
 
-      <div class="m-5">
-        <div
-          class="play-rate-block flex items-center flex-wrap"
-          v-if="nowInd != -1"
-        >
-          <h2>Скорость проигрывания:</h2>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(0.25)"
-            :class="{ active: playRate == 0.25 }"
-          >
-            0.25
+      <div class="m-5" v-if="video != null">
+        <div class="flex justify-start items-center space-x-[10px] flex-wrap">
+          <div class="flex flwx-wrap" v-if="quality.length > 0">
+            <select
+              class="bg-[#2b2b2b] p-[2px] rounded-[2px]"
+              name="quality"
+              v-model="selectQuality"
+            >
+              <option
+                @click="changeQuality(q.url)"
+                v-for="(q, ind) in quality"
+                :key="ind"
+                :value="q.name"
+              >
+                {{ q.name }}
+              </option>
+              <option
+                @click="changeQuality(anime.episodes[nowInd].url)"
+                value="1080p"
+              >
+                1080p
+              </option>
+            </select>
           </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(0.5)"
-            :class="{ active: playRate == 0.5 }"
+          <select
+            class="bg-[#2b2b2b] p-[2px] rounded-[2px]"
+            name="speed"
+            v-model="playRate"
           >
-            0.50
-          </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(0.75)"
-            :class="{ active: playRate == 0.75 }"
-          >
-            0.75
-          </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(1)"
-            :class="{ active: playRate == 1 }"
-          >
-            1
-          </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(1.25)"
-            :class="{ active: playRate == 1.25 }"
-          >
-            1.25
-          </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(1.5)"
-            :class="{ active: playRate == 1.5 }"
-          >
-            1.50
-          </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(1.75)"
-            :class="{ active: playRate == 1.75 }"
-          >
-            1.75
-          </div>
-          <div
-            class="ml-2 mr-2 cursor-pointer playrate"
-            @click="changePlayRate(2)"
-            :class="{ active: playRate == 2 }"
-          >
-            2
-          </div>
+            <option value="0.25">0.25x</option>
+            <option value="0.50">0.50x</option>
+            <option value="0.75">0.75x</option>
+            <option value="1">1x</option>
+            <option value="1.25">1.25x</option>
+            <option value="1.50">1.50x</option>
+            <option value="1.75">1.75x</option>
+            <option value="2">2x</option>
+          </select>
         </div>
+      </div>
+
+      <div class="m-5">
         <div class="episode-block" :class="{ active: video != null }">
           <div class="episode-scroll" :class="{ active: video != null }">
             <div
@@ -190,14 +168,14 @@
               :key="index"
               class="episode"
               :class="{ active: nowInd == index }"
-              @click="change(index, post.url, anime.title, post.chat)"
+              @click="change(index, post.url, anime.title, post.chat, post)"
             >
               <span>{{ post.title }}</span>
               <div class="right">
                 <i class="fas fa-comment-alt" v-if="post.chat"></i>
-                <a :href="post.url" class="down"
-                  ><i class="fas fa-download"></i
-                ></a>
+                <a :href="post.url" class="down">
+                  <i class="fas fa-download"></i>
+                </a>
               </div>
             </div>
           </div>
@@ -265,9 +243,9 @@
         <div class="arches-block">
           <div v-for="(seas, index) in anime.seasons" :key="index">
             {{ index + 1 }}.
-            <a class="link" target="_blank" :href="seas.url">{{
-              seas.title
-            }}</a>
+            <a class="link" target="_blank" :href="seas.url">
+              {{ seas.title }}
+            </a>
             - {{ seas.text }}
           </div>
         </div>
@@ -310,10 +288,10 @@ import Footer from "@/components/footer.vue";
 
 export default {
   async asyncData({ params, $axios }) {
-    let anime = await $axios.$get(
-      `https://olsior.herokuapp.com/api/anime?${params.name}`
-    );
-    // let anime = await $axios.$get(`/api/anime?${params.name}`);
+    // let anime = await $axios.$get(
+    //   `https://olsior.herokuapp.com/api/anime?${params.name}`
+    // );
+    let anime = await $axios.$get(`/api/anime?${params.name}`);
     anime = JSON.parse(anime);
     return { anime };
   },
@@ -344,6 +322,9 @@ export default {
       hideChat: false,
       playRate: 1,
       activeTab: "desc",
+      quality: [],
+      selectQuality: "1080p",
+      post: [],
     };
   },
   mounted() {
@@ -359,6 +340,18 @@ export default {
     }
   },
   computed: {},
+  watch: {
+    selectQuality(value) {
+      if (value == "480p") {
+        this.changeQuality(this.quality[0].url);
+      } else {
+        this.changeQuality(this.post.url);
+      }
+    },
+    playRate(value) {
+      if(this.$refs.video) this.$refs.video.playbackRate = Number(value);
+    },
+  },
   methods: {
     async getChat(url) {
       const emotes = await this.$axios.$get(
@@ -408,7 +401,17 @@ export default {
         });
       });
     },
-    change(ind, url, title, chat) {
+    changeQuality(url) {
+      if (this.save.id == this.nowInd) this.needSave = true;
+      this.video = url;
+    },
+    change(ind, url, title, chat, post) {
+      this.post = post;
+      if (post.quality) {
+        this.quality = post.quality;
+      } else {
+        this.quality = [];
+      }
       clearInterval(this.timer);
       this.chat = "";
       this.parsedChat = [];
@@ -454,10 +457,6 @@ export default {
       } else if (document.msExitFullscreen) {
         document.msExitFullscreen();
       }
-    },
-    changePlayRate(value) {
-      this.playRate = value;
-      this.$refs.video.playbackRate = this.playRate;
     },
     loadedVideo() {
       if (this.$refs.video == undefined) return;
@@ -559,6 +558,3 @@ export default {
   },
 };
 </script>
-
-<style>
-</style>
