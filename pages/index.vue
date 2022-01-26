81bc0ecd515264221936e9670109744a02838580
SGezha <sasha-gezha1@mail.ru>
2022-01-26 14:46:01 +0300

    heroku redirect

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,9 @@ export default {
       Footer
     };
   },
-  mounted() {},
+  mounted() {
+    document.location.href = document.location.href.split('olsior.herokuapp.com').join('smotrel.net')
+  },
   computed: {},
   methods: {}
 };
--- pages/support.vue
+++ pages/support.vue
@@ -142,6 +142,7 @@ export default {
     };
   },
   mounted() {
+    document.location.href = document.location.href.split('olsior.herokuapp.com').join('smotrel.net')
     this.donate = this.donate.reverse();
   },
   computed: {},
